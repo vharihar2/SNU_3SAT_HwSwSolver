4f65368cbeca56943a63eccc5dc6a241ab1403b6
SejalS01 <166379897+SejalS01@users.noreply.github.com>
2024-04-08 17:33:53 +0530

    Update and rename main code.py to AnalogSATsolverNetlistGenerator.py

--- AnalogSATsolverNetlistGenerator.py.py
+++ AnalogSATsolverNetlistGenerator.py.py
@@ -1,48 +1,25 @@
+"""
 ################################################################################
-
-# Copyright (c) 2024, Shiv Nadar University, Delhi NCR, India. All Rights
-
-# Reserved. Permission to use, copy, modify and distribute this software for
-
-# educational, research, and not-for-profit purposes, without fee and without a
-
-# signed license agreement, is hereby granted, provided that this paragraph and
-
-# the following two paragraphs appear in all copies, modifications, and
-
-# distributions.
-
-#
-
-# IN NO EVENT SHALL SHIV NADAR UNIVERSITY BE LIABLE TO ANY PARTY FOR DIRECT,
-
-# INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST
-
-# PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE.
-
-#
-
-# SHIV NADAR UNIVERSITY SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT
-
-# NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-
-# PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS PROVIDED "AS IS". SHIV
-
-# NADAR UNIVERSITY HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
-
-# ENHANCEMENTS, OR MODIFICATIONS.
-
-
-#Revision History:
-
-#      Date          By                     Change Notes
-
-# ------------  ---------------------- ------------------------------------------
-#  8 April 2024    Sejal Singh                 Netlist generation code
-
+Copyright (c) 2024, Shiv Nadar University, Delhi NCR, India. All Rights
+Reserved. Permission to use, copy, modify and distribute this software for
+educational, research, and not-for-profit purposes, without fee and without a
+signed license agreement, is hereby granted, provided that this paragraph and
+the following two paragraphs appear in all copies, modifications, and
+distributions.
+IN NO EVENT SHALL SHIV NADAR UNIVERSITY BE LIABLE TO ANY PARTY FOR DIRECT,
+INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST
+PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE.
+SHIV NADAR UNIVERSITY SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT
+NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS PROVIDED "AS IS". SHIV
+NADAR UNIVERSITY HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+Revision History:
+      Date          By                     Change Notes
+ ------------  ---------------------- ------------------------------------------
+  8 April 2024    Sejal Singh                 Netlist generation code
 ################################################################################
-
-
+"""
 def main():
     def input_matrix():
         # Input the size of the matrix
